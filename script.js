55d078b973b30c9dc9b5311f6413eb6abcfa26b9
Ben Tomazin <bentomazin@hotmail.com>
2022-05-28 22:04:20 +0200

    added grid squares turning black on mouseover

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const container = document.getElementById("container");
 
 
-let w = 16;
+let w = 64;
 
 for (let  i = 0; i< w; i++){
   for (let j = 0; j< w; j++){
     let square = document.createElement('div');
     square.setAttribute('class', 'square');
-    //square.textContent = "s";
     container.appendChild(square);
   }
 }
-
 container.style.setProperty('width', w *10 +"px");
+
+const grid = document.querySelectorAll(".square");
+
+grid.forEach(sq => {
+ 
+  sq.addEventListener("mouseover", e =>{
+    sq.style.setProperty('background-color', "black");
+    console.log(sq);
+    
+  });
+});
